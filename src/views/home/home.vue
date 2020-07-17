236498008d0e96d7f90e464bfa6095c0a0918bf2
Your NameFCX <13821739895@163.com>
2020-07-18 07:57:12 +0800

    20200718

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -15,12 +15,12 @@ import { mapGetters } from 'vuex'
 import { ADD_CART_ACTIONS } from '@/store/actions_types'
 import http from "network/home";
 import HeadBar from "components/common/headbar/HeadBar";
-// import Toast from 'components/common/toast/Toast'
+import Toast from 'components/common/toast/Toast'
 export default {
   name: "Home",
   components: {
     HeadBar,
-    // Toast
+    Toast
   },
   methods: {
     tesdMutaionsAndMutations() {
@@ -34,8 +34,8 @@ export default {
     http.getHomeData().then(data => { });
     this.$nextTick(() => { });
   },
-  mounted() {
-    // this.$toast.showToast('asdasd')
+  mounted () {
+    this.$toast.showToast('asdasd')
   },
   activated() {
     console.log('页面活跃');
@@ -51,7 +51,7 @@ export default {
 </script>
 
 <style lang="less">
-@import "~assets/less/test.less";
+@import '~assets/less/test.less';
 .test {
   .color();
 }
