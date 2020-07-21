5d0d3c9dde34b0ccfae574bc4f4b17c2344d9236
Your NameFCX <13821739895@163.com>
2020-07-21 23:03:06 +0800

    20200721

--- vue.simple.js
+++ vue.simple.js
@@ -1,44 +1,98 @@
 function Vue(options) {
+  debugger
   this._init(options)
 }
-Vue.prototype.$mount = function mount(el) {
-  const dom = document.querySelector(el)
-  //   const parentDom = 
-}
 
+Vue.prototype.$mount = function mount(el) {
+  this._el = el
+  const dom = document.querySelector(el)
+  const parent = dom.parentNode
+  console.dir(this.$el)
+  parent.appendChild(this.$el)
+}
 
 Vue.prototype._init = function init(opts) {
   const {
     data,
-    method,
+    methods,
     render
   } = opts
 
   function def(obj, key) {
+    const dep = new Dep()
     Object.defineProperty(obj, key, {
       get() {
-
+        debugger
+        dep.depend()
+        return obj[key]
       },
-      set() {
-
+      set(val) {
+        obj[key] = val
+        dep.notify()
       }
     })
   }
 
-  initData(vm, data) {
-    const data = vm.$data = data()
-    const keys = Object.keys(data)
-    key.forEach(item => {
-      def(data, key)
+  function initData(vm, data) {
+    const dataObj = vm.$data = data()
+    const keys = Object.keys(dataObj)
+    keys.forEach(key => {
+      def(dataObj, key)
     })
   }
 
-  initRender(vm, render) {
-
+  function initMethod(vm, methods) {
+    const keys = Object.keys(methods)
+    keys.forEach(key => {
+      methods[key] = methods[key].bind(vm)
+    })
   }
 
-  initMethod(vm, method) {
-
+  function initRender(vm, render) {
+    vm.$render = render
   }
 
+  initData(this, data)
+  initMethod(this, methods)
+  initRender(this, render)
+
+  function _render(vm) {
+    new Watcher(vm)
+  }
+  _render(this)
+}
+
+function Dep() {
+  this.subs = []
+}
+
+Dep.target = null
+
+Dep.prototype.notify = function notify() {
+  this.subs.forEach(item => {
+    item.notify()
+  })
+}
+
+Dep.prototype.depend = function depend() {
+  this.subs.push(Dep.target)
+}
+
+function createElm(tag, data, children) {
+  const dom = document.createElement(tag)
+  dom.innerText = data
+  return dom
+}
+
+function Watcher(vm) {
+  Dep.target = this
+  this.vm = vm
+  vm.$el = vm.$render(createElm)
+}
+
+Watcher.prototype.update = function () {
+  const data = this.vm.$data
+  const tag = this.vm.$el.nodeName
+  this.vm.$el = createElm(tag, data)
+  this.vm.$mount(this.vm._el)
 }
