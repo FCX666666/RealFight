70446d67240ecd05b9bd351f3274cb98f3565c43
FCX <2259585622@qq.com>
2020-07-22 09:00:50 +0800

    20200722

--- vue.simple.js
+++ vue.simple.js
@@ -1,13 +1,14 @@
 function Vue(options) {
-  debugger
   this._init(options)
 }
 
 Vue.prototype.$mount = function mount(el) {
   this._el = el
+  this.$el.id = this._el.replace('#', '')
   const dom = document.querySelector(el)
   const parent = dom.parentNode
-  console.dir(this.$el)
+  const children = parent.children
+  parent.removeChild(dom)
   parent.appendChild(this.$el)
 }
 
@@ -20,14 +21,16 @@ Vue.prototype._init = function init(opts) {
 
   function def(obj, key) {
     const dep = new Dep()
+    let val = obj[key]
     Object.defineProperty(obj, key, {
+      enumerable: true,
+      configurable: true,
       get() {
-        debugger
         dep.depend()
-        return obj[key]
+        return val
       },
-      set(val) {
-        obj[key] = val
+      set(value) {
+        val = value
         dep.notify()
       }
     })
@@ -46,6 +49,7 @@ Vue.prototype._init = function init(opts) {
     keys.forEach(key => {
       methods[key] = methods[key].bind(vm)
     })
+    vm.$method = methods
   }
 
   function initRender(vm, render) {
@@ -70,7 +74,7 @@ Dep.target = null
 
 Dep.prototype.notify = function notify() {
   this.subs.forEach(item => {
-    item.notify()
+    item.update()
   })
 }
 
@@ -79,6 +83,14 @@ Dep.prototype.depend = function depend() {
 }
 
 function createElm(tag, data, children) {
+  if (typeof data === 'object') {
+    let tp = ''
+    let keys = Object.keys(data)
+    keys.forEach(key => {
+      tp += data[key]
+    })
+    data = tp
+  }
   const dom = document.createElement(tag)
   dom.innerText = data
   return dom
